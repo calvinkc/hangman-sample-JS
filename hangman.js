2eddabd7796cc38ff0ef6cddf1b1e8acd4b373bb
Calvin <mrcalvinkc@gmail.com>
2019-05-30 17:43:47 -0700

    Fixed some typos, re-factored the getStatusMessage() for single return, spacings.

--- hangman.js
+++ hangman.js
@@ -6,44 +6,43 @@ class Hangman {
       this.status = 'playing';
   }
 }
+
 Hangman.prototype.calculateStatus = function () {
     const finished = this.word.every((letter) => this.guessedLetters.includes(letter))
-
     if (this.remainingGuesses === 0) {
-        this.status = 'failed'
+        this.status = 'failed';
     } else if (finished) {
-        this.status = 'finished'
+        this.status = 'finished';
     } else {
-        this.status = 'playing'
+        this.status = 'playing';
     }
 }
 
 Hangman.prototype.getPuzzle = function () {
-    let puzzle = ''
-
+    let puzzle = '';
     this.word.forEach((letter) => {
         if (this.guessedLetters.includes(letter) || letter === ' ') {
-            puzzle += letter
+            puzzle += letter;
         } else {
-            puzzle += '*'
+            puzzle += '*';
         }
     })
 
-    return puzzle
+    return puzzle;
 }
 
 Hangman.prototype.makeGuess = function (guess) {
     if (this.status == 'playing') {
-        guess = guess.toLowerCase()
-        const isUnique = !this.guessedLetters.includes(guess)
-        const isBadGuess = !this.word.includes(guess)
+        guess = guess.toLowerCase();
+        const isUnique = !this.guessedLetters.includes(guess);
+        const isBadGuess = !this.word.includes(guess);
 
         if (isUnique) {
-            this.guessedLetters.push(guess)
+            this.guessedLetters.push(guess);
         }
 
         if (isUnique && isBadGuess) {
-            this.remainingGuesses--
+            this.remainingGuesses--;
         }
 
         this.calculateStatus();
@@ -52,13 +51,23 @@ Hangman.prototype.makeGuess = function (guess) {
 }
 
 Hangman.prototype.getStatusMessage = function(){
+    // if (this.status === 'playing') {
+    //   return `Guesses left: ${this.remainingGuesses}`;
+    // } else if (this.status === 'failed') {
+    //   return `Nice try! The word was "${this.word.join('')}"`;
+    // } else {
+    //   return 'Great work! You\'ve guessed the word';
+    // }
+    let statusMessage = '';
     if (this.status === 'playing') {
-      return `Guesses left: ${this.remainingGuesses}`;
+      statusMessage = `Guesses left: ${this.remainingGuesses}`;
     } else if (this.status === 'failed') {
-      return `Nice try! The word was "${this.word.join('')}"`;
+      statusMessage = `Nice try! The word was "${this.word.join('')}"`;
     } else {
-      return 'Great work! You\'ve guessed the word';
+      statusMessage = 'Great work! You\'ve guessed the word';
     }
+
+    return statusMessage;
 }
 
 // Playing -> Gueses left: 3
