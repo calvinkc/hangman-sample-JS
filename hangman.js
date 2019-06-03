f083a0e5b4c7e95a69c4e23ddab1b518e4d701fe
Calvin <mrcalvinkc@gmail.com>
2019-06-03 00:25:25 -0700

    Added semi-colons

---
+++ hangman.js
@@ -0,0 +1,67 @@
+// convert getstatusmessage to custom getter for statusmessage
+// convert getpuzzle to a custom getter for puzzle
+// change usage in app js
+
+class Hangman {
+  constructor(word, remainingGuesses) {
+      this.word = word.toLowerCase().split('');
+      this.remainingGuesses = remainingGuesses;
+      this.guessedLetters = [];
+      this.status = 'playing';
+  }
+  calculateStatus() {
+    const finished = this.word.every((letter) => this.guessedLetters.includes(letter) || letter === ' ')
+
+    if (this.remainingGuesses === 0) {
+        this.status = 'failed';
+    } else if (finished) {
+        this.status = 'finished';
+    } else {
+        this.status = 'playing';
+    }
+  }
+  get StatusMessage() {
+    let statusMessage = '';
+
+    if (this.status === 'playing') {
+      statusMessage = `Guesses left: ${this.remainingGuesses}`;
+    } else if (this.status === 'failed') {
+      statusMessage = `Nice try! The word was "${this.word.join('')}"`;
+    } else {
+      statusMessage = 'Great work! You\'ve guessed the word';
+    }
+
+    return statusMessage;
+  }
+  get Puzzle() {
+    let puzzle = '';
+
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter;
+        } else {
+            puzzle += '*';
+        }
+    })
+
+    return puzzle;
+  }
+  makeGuess = function (guess) {
+    if (this.status == 'playing') {
+        guess = guess.toLowerCase();
+        const isUnique = !this.guessedLetters.includes(guess);
+        const isBadGuess = !this.word.includes(guess);
+
+        if (isUnique) {
+            this.guessedLetters.push(guess);
+        }
+        if (isUnique && isBadGuess) {
+            this.remainingGuesses--;
+        }
+
+        this.calculateStatus();
+
+    } 
+    else console.log('No longer playing');
+  }
+}
