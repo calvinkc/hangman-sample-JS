f23b146fc857728eb9ce2a4ae9fc511913645b7f
Calvin <mrcalvinkc@gmail.com>
2019-07-02 17:04:24 -0700

    API call to now work asynchronously

--- hangman.js
+++ hangman.js
@@ -1,71 +1,60 @@
-// convert getstatusmessage to custom getter for statusmessage
-// convert getpuzzle to a custom getter for puzzle
-// change usage in app js
-
 class Hangman {
-  constructor(word, remainingGuesses) {
-      this.word = word.toLowerCase().split('');
-      this.remainingGuesses = remainingGuesses;
-      this.guessedLetters = [];
-      this.status = 'playing';
-  }
-
-  calculateStatus() {
-    const finished = this.word.every((letter) => this.guessedLetters.includes(letter) || letter === ' ')
-
-    if (this.remainingGuesses === 0) {
-        this.status = 'failed';
-    } else if (finished) {
-        this.status = 'finished';
-    } else {
-        this.status = 'playing';
+    constructor(word, remainingGuesses) {
+        this.word = word.toLowerCase().split('')
+        this.remainingGuesses = remainingGuesses
+        this.guessedLetters = []
+        this.status = 'playing'
     }
-  }
+    calculateStatus() {
+        const finished = this.word.every((letter) => this.guessedLetters.includes(letter) || letter === ' ')
 
-  get StatusMessage() {
-    let statusMessage = '';
-
-    if (this.status === 'playing') {
-      statusMessage = `Guesses left: ${this.remainingGuesses}`;
-    } else if (this.status === 'failed') {
-      statusMessage = `Nice try! The word was "${this.word.join('')}"`;
-    } else {
-      statusMessage = 'Great work! You\'ve guessed the word';
-    }
-
-    return statusMessage;
-  }
-
-  get Puzzle() {
-    let puzzle = '';
-
-    this.word.forEach((letter) => {
-        if (this.guessedLetters.includes(letter) || letter === ' ') {
-            puzzle += letter;
+        if (this.remainingGuesses === 0) {
+            this.status = 'failed'
+        } else if (finished) {
+            this.status = 'finished'
         } else {
-            puzzle += '*';
+            this.status = 'playing'
         }
-    })
+    }
+    get statusMessage() {
+        if (this.status === 'playing') {
+            return `Guesses left: ${this.remainingGuesses}`
+        } else if (this.status === 'failed') {
+            return `Nice try! The word was "${this.word.join('')}".`
+        } else {
+            return 'Great work! You guessed the word.'
+        }
+    }
+    get puzzle() {
+        let puzzle = ''
 
-    return puzzle;
-  }
-  
-  makeGuess = function (guess) {
-    if (this.status == 'playing') {
-        guess = guess.toLowerCase();
-        const isUnique = !this.guessedLetters.includes(guess);
-        const isBadGuess = !this.word.includes(guess);
+        this.word.forEach((letter) => {
+            if (this.guessedLetters.includes(letter) || letter === ' ') {
+                puzzle += letter
+            } else {
+                puzzle += '*'
+            }
+        })
+
+        return puzzle
+    }
+    makeGuess(guess) {
+        guess = guess.toLowerCase()
+        const isUnique = !this.guessedLetters.includes(guess)
+        const isBadGuess = !this.word.includes(guess)
+
+        if (this.status !== 'playing') {
+            return
+        }
 
         if (isUnique) {
-            this.guessedLetters.push(guess);
+            this.guessedLetters.push(guess)
         }
+
         if (isUnique && isBadGuess) {
-            this.remainingGuesses--;
+            this.remainingGuesses--
         }
 
-        this.calculateStatus();
-
-    } 
-    else console.log('No longer playing');
-  }
+        this.calculateStatus()
+    }
 }
--- requests.js
+++ requests.js
@@ -1,32 +1,15 @@
-const getPuzzle = () => {
-  return 'what the fizzle';
+const getPuzzle = (callback) => {
+    const request = new XMLHttpRequest()
+
+    request.addEventListener('readystatechange', (e) => {
+        if (e.target.readyState === 4 && e.target.status === 200) {
+            const data = JSON.parse(e.target.responseText)
+            callback(undefined, data.puzzle)
+        } else if (e.target.readyState === 4) {
+            callback('An error has taken place', undefined)
+        }
+    })
+
+    request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
+    request.send()
 }
-
-const request = new XMLHttpRequest();
-request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText);
-        console.log(data);
-    } else if (e.target.readyState === 4) {
-        console.log("An error has occured.");
-    }
-    
-})
-request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3');
-request.send();
-
-
-const newRequest = new XMLHttpRequest();
-newRequest.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status == 200) {
-        const data = JSON.parse(e.target.responseText);
-        const answer = data.find((country) => country.alpha2Code === countryCode);
-        answer ? console.log(answer.name) : console.log('Nope such country');
-   //     console.log(data);
-    } else if (e.target.readyState === 4) {
-        console.log('Some error occured..');
-    }
-})
-newRequest.open('get', 'http://restcountries.eu/rest/v2/all');
-newRequest.send();
-const countryCode = "US";
