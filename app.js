c36cccb34f02c0f7c05cb8f7355e380b1a1ae0b7
Calvin <mrcalvinkc@gmail.com>
2019-07-09 18:52:23 -0700

    Converted callbacks into Promises

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
-const puzzleEl = document.querySelector('#puzzle');
-const guessesEl = document.querySelector('#guesses');
-const game1 = new Hangman('car parts', 10);
+const puzzleEl = document.querySelector('#puzzle')
+const guessesEl = document.querySelector('#guesses')
+const game1 = new Hangman('Car Parts', 2)
 
-puzzleEl.textContent = game1.Puzzle;
-guessesEl.textContent = game1.StatusMessage;
+puzzleEl.textContent = game1.puzzle
+guessesEl.textContent = game1.statusMessage
 
 window.addEventListener('keypress', (e) => {
-    const guess = String.fromCharCode(e.charCode);
-    game1.makeGuess(guess);
-    puzzleEl.textContent = game1.Puzzle;
-    guessesEl.textContent = game1.StatusMessage;
+    const guess = String.fromCharCode(e.charCode)
+    game1.makeGuess(guess)
+    puzzleEl.textContent = game1.puzzle
+    guessesEl.textContent = game1.statusMessage
 })
 
-const newRequest = new XMLHttpRequest();
-newRequest.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status == 200) {
-        const data = JSON.parse(e.target.responseText);
-        const answer = data.find((country) => country.alpha2Code === countryCode);
-        answer ? console.log(answer.name) : console.log('Nope such country');
-        console.log(data);
-    } else if (e.target.readyState === 4) {
-        console.log('Some error occured..');
-    }
+getPuzzle('2').then((puzzle) => {
+    console.log(puzzle)
+}, (err) => {
+    console.log(`Error: ${err}`)
 })
-newRequest.open('get', 'http://restcountries.eu/rest/v2/all');
-newRequest.send();
-const countryCode = "US";
+
+getCountry('MX').then((country) => {
+    console.log(country.name)
+}, (err) => {
+    console.log(`Error: ${err}`)
+})
